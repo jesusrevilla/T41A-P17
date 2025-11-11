7729e022297633b2da27309c5f5d15aab4166852
Uriel4443 <182829@upslp.edu.mx>
2025-11-10 19:42:59 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -5,3 +5,11 @@ VALUES
   ('{"nombre": "Jhon", "activo": true, "edad": 20}'),
   ('{"nombre": "Sarah", "activo": false, "edad": 21}'),
   ('{"nombre": "Steve", "activo": false, "edad": 24}');
+
+INSERT INTO productos (nombre, atributos)
+VALUES 
+  ('Jamón', 'marca => Fud, color => rosa, peso => 0.5kg'),
+  ('Manzana', 'marca => Villa, color => rojo, peso => 1Kg'),
+  ('Jugo', 'marca => Jumex, color => azul, peso => 0.5kg'),
+  ('Tomate', 'marca => Costeña, color => rojo, peso => 0.7Kg'),
+  ('Refresco', 'marca => Escuis, color => amarillo, peso => 0.6Kg');
