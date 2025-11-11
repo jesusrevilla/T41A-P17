806eb3634a40af52c130e56c6607c28c110c38ce
Uriel4443 <182829@upslp.edu.mx>
2025-11-10 19:33:59 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,4 +1,7 @@
 INSERT INTO usuarios (data)
 VALUES 
   ('{"nombre": "Ana", "activo": true, "edad": 30}'),
-  ('{"nombre": "Juan", "activo": false, "edad": 25}');
+  ('{"nombre": "Juan", "activo": false, "edad": 25}'),
+  ('{"nombre": "Jhon", "activo": true, "edad": 20}'),
+  ('{"nombre": "Sarah", "activo": false, "edad": 21}'),
+  ('{"nombre": "Steve", "activo": false, "edad": 24}');
