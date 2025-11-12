6f2e0e5e57c17a8c93d7168a2d5faad316b949ad
Palau003 <178561@upslp.edu.mx>
2025-11-11 19:24:50 -0600

    Update 07_insert_hstore_data.sql

--- 07_insert_hstore_data.sql
+++ 07_insert_hstore_data.sql
@@ -1,7 +1,8 @@
+DELETE FROM productos;
 INSERT INTO productos (nombre, atributos)
-VALUES 
-  ('Laptop', 'marca => Dell, color => negro, ram => 16GB'),
-  ('Teléfono', 'marca => Samsung, color => azul, ram => 8GB'),
+VALUES
+  ('Teléfono', 'color => "azul", marca => "Samsung"'),
+  ('Laptop', 'ram => "16GB", marca => "Dell"');
   ('Tablet', 'marca => Apple, color => gris, ram => 4GB'),
   ('Monitor', 'marca => LG, color => negro, ram => 2GB'),
   ('Smartwatch', 'marca => Huawei, color => blanco, ram => 1GB');
