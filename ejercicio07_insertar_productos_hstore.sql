359227e3bb6441f02d45a23178c55580c150c0d5
Karina123Kma <upslpbd@gmail.com>
2025-11-11 19:16:24 -0600

    Create ejercicio07_insertar_productos_hstore.sql

---
+++ ejercicio07_insertar_productos_hstore.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos_hstore (nombre, atributos) VALUES
+('Laptop', 'marca=>"Dell", color=>"gris", peso=>"2kg"'),
+('Celular', 'marca=>"Samsung", color=>"negro", peso=>"200g"'),
+('Camiseta', 'marca=>"Nike", color=>"rojo", peso=>"150g"'),
+('Mesa', 'marca=>"IKEA", color=>"marrón", peso=>"15kg"'),
+('Auriculares', 'marca=>"Sony", color=>"blanco", peso=>"250g"');
