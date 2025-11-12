fc9633292a3324d0c6e521e6cca3543cb35d433f
Karina123Kma <upslpbd@gmail.com>
2025-11-11 19:14:23 -0600

    Create ejercicio02_insertar_productos_jsonb.sql

---
+++ ejercicio02_insertar_productos_jsonb.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos_jsonb (nombre, especificaciones) VALUES
+('Laptop', '{"color": "gris", "tamaño": "15 pulgadas", "categoria": "Electrónica"}'),
+('Celular', '{"color": "negro", "tamaño": "6 pulgadas", "categoria": "Electrónica"}'),
+('Camiseta', '{"color": "rojo", "tamaño": "M", "categoria": "Ropa"}'),
+('Mesa', '{"color": "marrón", "tamaño": "1.5m", "categoria": "Muebles"}'),
+('Auriculares', '{"color": "blanco", "categoria": "Accesorios"}');
