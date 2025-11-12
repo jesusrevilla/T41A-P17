e32e0dad631516736bd3d39b320b8338bc5e763a
Karina123Kma <upslpbd@gmail.com>
2025-11-11 19:50:20 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -8,3 +8,13 @@ CREATE TABLE productos_jsonb (
 );
 
 
+CREATE TABLE IF NOT EXISTS productos_hstore (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    atributos HSTORE
+);
+
+-- Crear índice GIN si es necesario
+CREATE INDEX IF NOT EXISTS idx_productos_hstore_atributos ON productos_hstore USING GIN (atributos);
+
+CREATE INDEX IF NOT EXISTS idx_productos_jsonb_especificaciones ON productos_jsonb USING GIN (especificaciones);
