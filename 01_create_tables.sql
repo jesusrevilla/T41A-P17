caa16a051438e6057098f068e9e74bdd422977e4
VictorCastro135 <178920@upslp.edu.mx>
2025-11-12 13:40:04 -0600

    feature/178920

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -2,3 +2,10 @@ CREATE TABLE usuarios (
   id SERIAL PRIMARY KEY,
   data JSONB
 );
+
+
+CREATE IF NOT EXISTS TABLE productos_jsonb (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100),
+  especificaciones JSONB
+);
