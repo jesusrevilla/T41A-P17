ab7d5cadee89d8f1f01f191ef7387d22944fe7b3
Karina123Kma <upslpbd@gmail.com>
2025-11-12 16:26:04 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,8 +1,9 @@
-SELECT data->>'nombre' AS nombre
+SELECT id, data->>'nombre' AS nombre, data->>'edad' AS edad
 FROM usuarios
 WHERE data->>'activo' = 'true';
 
 CREATE INDEX idx_data_gin ON usuarios USING GIN (data);
 
-SELECT * FROM usuarios
+SELECT id, data
+FROM usuarios
 WHERE data @> '{"activo": true}';
