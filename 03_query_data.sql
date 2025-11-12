94ec896d63fe0b957ac0a018af8f3cad39c7527f
Karina123Kma <upslpbd@gmail.com>
2025-11-11 19:11:08 -0600

    Delete 03_query_data.sql

--- 03_query_data.sql
+++
@@ -1,8 +0,0 @@
-SELECT data->>'nombre' AS nombre
-FROM usuarios
-WHERE data->>'activo' = 'true';
-
-CREATE INDEX idx_data_gin ON usuarios USING GIN (data);
-
-SELECT * FROM usuarios
-WHERE data @> '{"activo": true}';
