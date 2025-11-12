61a6c6d1ae8c0a55083345d9732a5f47feea5fea
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-11-11 19:37:05 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -2,3 +2,10 @@ INSERT INTO usuarios (data)
 VALUES 
   ('{"nombre": "Ana", "activo": true, "edad": 30}'),
   ('{"nombre": "Juan", "activo": false, "edad": 25}');
+
+INSERT INTO productos (nombre, atributos) VALUES
+  ('Laptop', 'marca => Dell, color => negro, ram => 16GB'),
+  ('Teléfono', 'marca => Samsung, color => azul, ram => 8GB'),
+  ('Tablet', 'marca => Apple, color => blanco, ram => 4GB'),
+  ('Monitor', 'marca => LG, color => negro, tamaño => 24"'),
+  ('Impresora', 'marca => HP, color => blanco, peso => 5kg');
