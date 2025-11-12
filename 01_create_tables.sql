85b4d218e0143174de1811663c8c47089052e074
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-11-11 19:36:38 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,4 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS hstore;
 CREATE TABLE usuarios (
   id SERIAL PRIMARY KEY,
   data JSONB
+);
+CREATE TABLE productos (
+  id SERIAL PRIMARY KEY,
+  nombre TEXT,
+  atributos HSTORE
 );
