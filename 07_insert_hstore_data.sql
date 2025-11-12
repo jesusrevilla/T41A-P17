6ffe920d979a7d5981fe4bdd7c23eed0bf6bdfb5
Palau003 <178561@upslp.edu.mx>
2025-11-11 19:19:18 -0600

    Create 07_insert_hstore_data.sql

---
+++ 07_insert_hstore_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO productos (nombre, atributos)
+VALUES 
+  ('Laptop', 'marca => Dell, color => negro, ram => 16GB'),
+  ('Teléfono', 'marca => Samsung, color => azul, ram => 8GB'),
+  ('Tablet', 'marca => Apple, color => gris, ram => 4GB'),
+  ('Monitor', 'marca => LG, color => negro, ram => 2GB'),
+  ('Smartwatch', 'marca => Huawei, color => blanco, ram => 1GB');
