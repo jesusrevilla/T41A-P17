8283e50a7dbca37bcb7fe01c0122a5a419d9b13a
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-15 19:25:28 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS hstore;
+
 CREATE TABLE usuarios (
   id SERIAL PRIMARY KEY,
   data JSONB
@@ -6,4 +8,9 @@ CREATE TABLE usuarios (
 CREATE TABLE productos (
   id SERIAL PRIMARY KEY,
   especificaciones JSONB
+);
+
+CREATE TABLE productos1 (
+  id SERIAL PRIMARY KEY,
+  atributos HSTORE
 );
