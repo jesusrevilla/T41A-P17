7a5624c79088b1fbc1b99d55317bad4793070a33
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-15 19:16:30 -0600

    Update 04_unit_tests.sql

--- 04_unit_tests.sql
+++ 04_unit_tests.sql
@@ -23,5 +23,29 @@ BEGIN
   ELSE
     RAISE EXCEPTION 'Fallo: edad incorrecta para id 2';
   END IF;
+----------------------------------------------------------------------------------
+  IF EXISTS (
+    SELECT 1 FROM productos WHERE id = 1 AND especificaciones->>'color' = 'Verde'
+  ) THEN
+    RAISE NOTICE 'OK: color correcto para id 1';
+  ELSE
+    RAISE EXCEPTION 'Fallo: color incorrecto para id 1';
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM productos WHERE id = 1 AND especificaciones->>'Tamaño' = 'Pequeño'
+  ) THEN
+    RAISE NOTICE 'OK: objeto pequeño para id 1';
+  ELSE
+    RAISE EXCEPTION 'Fallo: objeto no es pequeño para id 1';
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM productos WHERE id = 2 AND especificaciones->>'Categoría' = 'Electrodoméstico'
+  ) THEN
+    RAISE NOTICE 'OK: categoría correcta para id 2';
+  ELSE
+    RAISE EXCEPTION 'Fallo: categoría incorrecta para id 2';
+  END IF;
 END;
 $$;
