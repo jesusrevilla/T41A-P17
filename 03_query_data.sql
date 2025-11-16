4d10e6714597aff6f12aca7e9329a476121b8bd6
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-15 19:34:27 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -10,3 +10,15 @@ WHERE data @> '{"activo": true}';
 SELECT especificaciones->>'color' AS color, especificaciones->>'Tamaño' AS tamaño, especificaciones->>'Categoría' AS categoría
 FROM productos;
 CREATE INDEX idx_productos_gin ON productos USING GIN (especificaciones);
+
+SELECT *
+FROM productos1
+WHERE atributos -> 'color' = 'rojo';
+
+UPDATE productos1
+SET atributos = atributos || 'peso => 10kg'
+WHERE id = 1;
+
+UPDATE productos1
+SET atributos = delete(atributos, 'color')
+WHERE id = 2;
