1431bb31c4dc53c534c18663bcb899a36e6018d5
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-15 19:08:36 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -6,3 +6,7 @@ CREATE INDEX idx_data_gin ON usuarios USING GIN (data);
 
 SELECT * FROM usuarios
 WHERE data @> '{"activo": true}';
+
+SELECT data->>'Color' AS color, data->>'Tamaño' AS tamaño, data->>'Categoría' AS categoría
+FROM productos;
+CREATE INDEX idx_productos_gin ON productos USING GIN (data);
