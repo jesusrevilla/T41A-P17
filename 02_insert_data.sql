838d18660738c1e6868fd93c6da7ffc28a9ec9c6
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-15 19:05:15 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -2,3 +2,11 @@ INSERT INTO usuarios (data)
 VALUES 
   ('{"nombre": "Ana", "activo": true, "edad": 30}'),
   ('{"nombre": "Juan", "activo": false, "edad": 25}');
+
+INSERT INTO productos (data)
+VALUES 
+  ('{"color": "Verde", "Tamaño": "Grande", "Categoría": "Electrodoméstico"}'),
+  ('{"color": "Rojo", "Tamaño": "Mediano", "Categoría": "Comida"}'),
+  ('{"color": "Amarillo", "Tamaño": "Pequeño", "Categoría": "Ocio"}'),
+  ('{"color": "Azul", "Tamaño": "Intermedio", "Categoría": "Música"}'),
+  ('{"color": "Gris", "Tamaño": "Enorme", "Categoría": "Utilidad"}');
